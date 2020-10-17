bf534f265f27aa8b7df7fcdf20f49c8b0ba0c5ba
Luca Fanselau <luca.fanselau@outlook.com>
2020-10-17 09:03:33 +0200

    :children_crossing: add prelude with most used features of ecs

--- crates/ecs/src/lib.rs
+++ crates/ecs/src/lib.rs
@@ -2,9 +2,16 @@
 // if there are drawbacks, we might need to remove that
 #![feature(trait_alias)]
 
-mod resource;
-mod system;
-mod schedule;
+pub mod resource;
+pub mod schedule;
+pub mod system;
+
+pub mod prelude {
+    pub use crate::resource::{ResourceQuery, Resources};
+    pub use crate::schedule::{executor::SequentialExecutor, scheduler::Scheduler};
+    pub use crate::system::{into_system::IntoFunctionSystem, System};
+    pub use hecs::{QueryBorrow, World};
+}
 
 // This whole thing is largely based on the bevy_ecs, since it seems to be quite a good ecs.
 // maybe it would be easier to just pull in the bevy_ecs, but i guess here we are
